4e66c4a314d42a1b0a974a2aeec574b911abe2ef
rejoncanul01 <65475813+rejoncanul01@users.noreply.github.com>
2023-11-22 15:11:31 -0600

    upload files

--- js/register-service-wroker.js
+++ js/register-service-wroker.js
@@ -1,7 +1,7 @@
 <script>
-        if ('ServiceWorker' in navigator) {
-            navigator.ServiceWorker.register('service-worker.js')
-                .then(registration => console.log('Service Worker registrado con exito:', registration))
-                .catch(error => console.error('Error al registrar el service worker', error));
-        }
-  </script>
+    if ('ServiceWorker' in navigator) {
+        navigator.ServiceWorker.register('service-worker.js')
+            .then(registration => console.log('Service Worker registrado con exito:', registration))
+            .catch(error => console.error('Error al registrar el service worker', error))
+    }
+</script>
