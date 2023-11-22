0fa4f24512ddcf7f43eb658ee8ee9c275ea76bbe
rejoncanul01 <65475813+rejoncanul01@users.noreply.github.com>
2023-11-22 13:15:11 -0600

    Delete assets/css/js directory

--- assets/css/js/script.js
+++
@@ -1,5 +0,0 @@
-
-var formulario_login = document.querySelector(".formulario_login")
-var formulario_registro = document.querySelector("formulario_registro")
-var contenedor_login = document.querySelector(".contenedor_login");
-var contenedor_registro = document.querySelector(".contenedor_registro");
